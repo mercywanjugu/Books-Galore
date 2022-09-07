3a8c0f258d38e868f4cc3e5e4558919398ab4637
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-09-07 15:27:33 +0300

    initial commit

---
+++ books-galore/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar';
+// import Home from "./Home";
+// import Contact from "./Contact";
+// import About from "./About";
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar />
+    </div>
+  );
+}
+
+export default App;
