1206c729c9234b7fad82f59af8832cf20fae49ab
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-09-07 21:24:13 +0300

    updated App.js

--- books-galore/src/App.js
+++ books-galore/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import Navbar from './components/Navbar';
-// import Contact from "./Contact";
+ import Contact from "./Contact";
 import About from "./About";
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [author,setAuthor] = useState([]);
+  const [category,setCategory] = useState([]);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+    function handleDarkModeClick() {
+      setIsDarkMode((isDarkMode) => !isDarkMode);
+    }
   return (
+    
     <div className="App">
         <Navbar />
+        <About/>
+        <Contact/>
     </div>
   );
 }
